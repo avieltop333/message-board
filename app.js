d17f7d61e4ee7789c1382f1a06b58ee9cf48dadd
avieltop333 <avieltan123@gmail.com>
2025-06-04 22:05:19 +0800

    msg board app

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ const formRouter = require("./routes/formRouter");
 
 const app = express();
 
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 3000;
 
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
